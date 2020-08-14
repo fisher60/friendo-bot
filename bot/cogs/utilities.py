ea45dd91543a7e0f7c8f14dd63bcc08cdc54f8da
fisher60 <wakefollower@gmail.com>
2020-08-13 19:19:24 -0700

    added reminder command

--- bot/cogs/utilities.py
+++ bot/cogs/utilities.py
@@ -1,10 +1,68 @@
+import datetime
+from asyncio import sleep
+from discord.ext import tasks
 from discord.ext.commands import Bot, Cog, command
+from discord.utils import sleep_until
 from bot import settings
+
+
+def convert_time(time, period):
+    result = None
+    try:
+        time = int(time)
+        if "sec" in period:
+            result = time
+        elif "min" in period:
+            result = time * 60
+        elif "hour" in period:
+            result = time * (60**2)
+    except ValueError:
+        pass
+    return result
+
+# def convert_time(time, period):
+#     result = None
+#     try:
+#         time = int(time)
+#         if "sec" in period:
+#             result = datetime.timedelta(seconds=time)
+#         elif "min" in period:
+#             result = datetime.timedelta(seconds=time * 60)
+#         elif "hour" in period:
+#             result = datetime.timedelta(seconds=time * (60**2))
+#     except ValueError:
+#         pass
+#     return datetime.datetime.now() + result
+
 
 
 class Utilities(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
+        self.index = 0
+
+    async def reminder_wrapper(self, time, period, ctx, msg="Reminder!"):
+
+        @tasks.loop(count=1)
+        async def create_reminder():
+            seconds = convert_time(time, period)
+            if not seconds:
+                msg = "Please enter a valid time and period (i.e .reminder 5 minutes)"
+                await ctx.send(msg)
+                return msg
+            else:
+                await sleep(seconds)
+            return
+
+        @create_reminder.after_loop
+        async def after_create_reminder():
+            await ctx.send(msg)
+
+        create_reminder.start()
+
+    # async def create_reminder(self, ctx, time, period, msg="Reminder!"):
+    #     await sleep_until(convert_time(time, period), await ctx.send(msg))
+
 
     @command()
     async def version(self, ctx):
@@ -13,10 +71,9 @@ class Utilities(Cog):
         return msg
 
     @command()
-    async def version(self, ctx):
-        msg = f"Version is {settings.VERSION}"
-        await ctx.send(msg)
-        return msg
+    async def reminder(self, ctx, time, period="minutes"):
+        await self.reminder_wrapper(ctx=ctx, time=time, period=period)
+        return
 
 
 def setup(bot: Bot) -> None:
