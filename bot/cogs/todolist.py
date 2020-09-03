3d14252d553d620558a4f6a7eeaa0ebd88d88143
Soc Virnyl Estela <66054069+ReneganRonin@users.noreply.github.com>
2020-09-03 14:17:42 +0800

    New feature to commit for review

--- bot/cogs/todolist.py
+++ bot/cogs/todolist.py
@@ -1,4 +1,5 @@
 from asyncio import sleep
+from settings import BASE_DIR
 from discord.ext import tasks
 from discord.ext.commands import Bot, Cog, command
 import json
@@ -11,14 +12,16 @@ class TodoList(Cog):
         self.bot = bot
         self.index = 0
         self.todos = {}
+        self.del_todos = {}
         self.the_todo = []
 
     async def todos_wrapper(
-            self, ctx, text, msg="Todo List!", task_type="todo_list", reason=None
+        self, ctx, text, msg="Todo List!", task_type="todolist", reason=None
     ):
-        if task_type == "todo_list":
+        if task_type == "todolist":
             self.todos[ctx.author.id] += 1
-
+        elif task_type == "del_todolist":
+            self.del_todos[ctx.author.id] += 1
         @tasks.loop(count=1)
         async def create_todo_list():
             """Sets a delay for the todo_list to complete."""
@@ -28,48 +31,101 @@ class TodoList(Cog):
         @create_todo_list.after_loop
         async def after_create_todo_list():
             """After the delay is complete, it will message
-            the user that his todo list is ready."""
+            the user that his todo list is ready (or a todo is deleted in the todolist)."""
 
             completion_message = msg
 
-            if task_type == 'todo_list':
+            if task_type == "todolist":
+                
                 if reason:
-                    r = reason.split(',')
-                    for num, td in enumerate(r):
-                        self.the_todo.append(f"{num}. {td}\n")
-                    completion_message = (
+                    r = reason.split(",")
+                    
+                    for td in r:
+                        self.the_todo.append(f"{td}\n")
+                    
+                    with open(
+                        f"{BASE_DIR}save_data_td.JSON", "r+"
+                    ) as read:  # This will read and convert json to dict.
+                        file = json.load(read)
+                        file.update([(ctx.author.id, self.the_todo)])
+                
+                        with open(
+                            f"{BASE_DIR}/save_data_td.JSON", "w+"
+                        ) as save:  # This will save the dict to json file.
+                            json.dump(file, save)
+                    
+                            completion_message = (
                         f"{ctx.author.mention}, your todo list : {''.join(self.the_todo) + ' -> Ready!' }"
                     )
+                
                 else:
                     completion_message = (
-                        f"{ctx.author.mention}, your todo list is empty. "
-                        f"Please enter a todo list format (i.e .todo Make assignments, Learn Python) with comma on "
+                        f"{ctx.author.mention}, your todo list entry is empty. "
+                        f"Please enter a todo list format (i.e `.todolist Make assignments, Learn Python`) with comma on "
                         f"every todo."
                     )
+                
                 self.todos[ctx.author.id] -= 1
+
+            elif task_type == 'del_todolist':
+                with open(f'{BASE_DIR}/save_data_td.JSON', 'r+') as read:
+                    file = json.load(read)
+                    r = reason.split(',')
+                    if file[ctx.author.id]:
+                        exists = [e + '\n' for e in file[ctx.author.id] if e + '\n' in r]
+                        not_exists = [ne + '\n' for ne in r if ne + '\n' not in file[ctx.author.id]]
+                    
+                        for _ in exists:
+                            file[ctx.author.id].__delitem__(file[ctx.author.id].index(_))
+                    
+                        with open(f'{BASE_DIR}/save_data_td.JSON', 'w+') as save:
+                            json.dump(file, save)
+                    
+                        completion_message = (
+                            f"{ctx.author.mention}, you have deleted the following todos: {''.join(not_exists)}\n"
+                            f"The following entry or entries don't exist: {''.join(not_exists)}"
+                        )
+
+                    else:
+                        completion_message = (
+                            f"{ctx.author.mention}, you have no todos to delete. Maybe you made a mistake?"
+                        )
+                
+                self.del_todolist[ctx.author.id] -= 1
 
             await ctx.send(completion_message)
 
-        if text.startswith("todo"):
+        if text.startswith("todolist") or text.startswith('del_todolist'):
             create_todo_list.start()
         else:
-            msg = "Please enter a todo list format (i.e .todo Make assignments, Learn Python) with comma on every todo."
+            msg = """Please enter a todo list format (i.e `.todo Make assignments, Learn Python`) with comma on every todo.
+            To delete a todo in your todolist do the example: `.del_todolist Make assignments, Learn Python.`"""
             await ctx.send(msg)
 
+
+    @async del_todo_list_wrapper(
+        self, ctx, text, msg="Delete Todo in Todo List!", task_type="del_todo_list", reason=None
+    ):
+
     @command(brief="[todo some todos] [reason]")
-    async def todo_list(self, ctx, text, reason=None):
-        """Creates a todo list for the user"""
+    async def todolist(self, ctx, text, reason=None):
+        """Creates a todo list for the user."""
         if ctx.author.id not in self.todos:
             self.todos[ctx.author.id] = 0
         if self.todos[ctx.author.id] < 1:
             await self.todos_wrapper(
-                ctx=ctx, text=text, task_type='todo_list', reason=reason
+                ctx=ctx, text=text, task_type="todolist", reason=reason
             )
 
-    @command(brief="[delete todo some todos] [reason]")
-    async def del_todo_list(self, ctx, text, reason=None):
-        ...
-
+    @command(brief="[del_todolist some todos] [reason]")
+    async def del_todolist(self, ctx, text, reason=None):
+        """Deletes a todo in the todolist for the user."""
+        if ctx.author.id not in self.del_todos:
+            self.del_todos[ctx.author.id] = 0
+        if self.del_todos[ctx.author.id] < 1:
+            await self.todos_wrapper(
+                ctx=ctx, text=text, task_type="del_todolist",reason=reason
+            )
 
 def setup(bot: Bot) -> None:
     """Load the Todo_List cog."""
