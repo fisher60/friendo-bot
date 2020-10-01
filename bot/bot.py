0de1869ece6ca261224a8dfb09ed70b42005c934
Xithrius <xithrius@gmail.com>
2020-09-30 19:32:56 -0700

    Created bot loop, session, and moved on_ready from __main__.py
    Also moved "name" into settings.py, since it's a constant.

--- bot/bot.py
+++ bot/bot.py
@@ -1,17 +1,30 @@
-"""Bot class functionality"""
+import asyncio
 
+import aiohttp
 from discord.ext import commands
+
+from .settings import NAME
 
 
 class Bot(commands.Bot):
-    """
-    Base Class for the discord bot
-    """
-
-    name = "Friendo"
+    """Base Class for the discord bot."""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        # Setting the loop.
+        self.loop = asyncio.get_event_loop()
+
+        # Creating session for web requests.
+        self.session = aiohttp.ClientSession()
+
     def __str__(self):
-        return self.name
+        """Returns the name of the bot."""
+        return NAME
+
+    async def on_ready(self):
+        """Runs when the bot is connected."""
+        print("Logged in as")
+        print(self.user.name)
+        print(self.user.id)
+        print("------")
