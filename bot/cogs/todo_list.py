64dbebc75c0fbd2b3ea58fae5caf6b61b8dea617
Soc Virnyl Estela <66054069+ReneganRonin@users.noreply.github.com>
2020-10-03 21:01:30 +0800

    Update: Finalizing feature for develop

--- bot/cogs/todo_list.py
+++ bot/cogs/todo_list.py
@@ -11,8 +11,10 @@ import os
 def update_of_todos(ctx, todos):
     """This will create a todo list from todos"""
 
+    todos = [t.strip() for t in todos.split(",") if t.strip() != ""]
+
     if Path(f"{BASE_DIR}/todo_list_data.json").is_file():  # Checks file if it exists
-        print("File exist")
+        print("File exists")
 
         if (
             os.stat(f"{BASE_DIR}/todo_list_data.json").st_size > 0
@@ -26,7 +28,7 @@ def update_of_todos(ctx, todos):
                 ) not in todo_file_read:  # If key does not exist, a new dictionary is created
                     todo_file_read[str(ctx.author.id)] = {
                         num: todo.strip().rstrip()
-                        for num, todo in enumerate(todos.split(","), start=1)
+                        for num, todo in enumerate(todos, start=1)
                     }
 
                 else:  # If key exists, this will auto merge the current dictionary with the new dictionary
@@ -35,7 +37,7 @@ def update_of_todos(ctx, todos):
                         **{
                             num: todo.strip().rstrip()
                             for num, todo in enumerate(
-                                todos.split(","),
+                                todos,
                                 start=len(todo_file_read[str(ctx.author.id)]) + 1,
                             )
                         },
@@ -43,7 +45,6 @@ def update_of_todos(ctx, todos):
 
             with open(f"{BASE_DIR}/todo_list_data.json", "w") as update_write:
                 # Writes newly updated dictionary and dump it to a json file.
-                print(todo_file_read)
                 json.dump(todo_file_read, fp=update_write)
 
         elif (
@@ -56,7 +57,7 @@ def update_of_todos(ctx, todos):
                     todo_file_read = json.load(read)
                     todo_file_read[str(ctx.author.id)] = {
                         num: todo.strip().rstrip()
-                        for num, todo in enumerate(todos.split(","), start=1)
+                        for num, todo in enumerate(todos, start=1)
                     }
                     json.dump(todo_file_read, fp=update_write)
 
@@ -66,10 +67,47 @@ def update_of_todos(ctx, todos):
             todo_dict = {
                 ctx.author.id: {
                     num: todo.strip().rstrip()
-                    for num, todo in enumerate(todos.split(","), start=1)
+                    for num, todo in enumerate(todos, start=1)
                 }
             }
             json.dump(todo_dict, fp=to_write)
+
+
+def deletion_of_todos(
+    ctx, keys_to_delete
+):  # In this function, todos here refers to keys in the dictionary.
+    """This will delete some todos using specified keys"""
+
+    k = [c.strip().rstrip() for c in keys_to_delete.split(",") if c.strip() != ""]
+    if Path(f"{BASE_DIR}/todo_list_data.json").is_file():
+        print("File exists")
+        if (
+            os.stat(f"{BASE_DIR}/todo_list_data.json").st_size > 0
+        ):  # This will check if the file is NOT empty
+            with open(f"{BASE_DIR}/todo_list_data.json", "r") as read:
+                todo_file_read = json.load(read)
+                if (
+                    str(ctx.author.id)
+                ) in todo_file_read:  # If key does not exist, a new dictionary is created
+                    for _ in k:
+                        if _ in todo_file_read[str(ctx.author.id)]:
+                            todo_file_read[str(ctx.author.id)].pop(_)
+
+                    todo_file_read[str(ctx.author.id)] = {
+                        num: todo
+                        for num, todo in enumerate(
+                            todo_file_read[str(ctx.author.id)].values(), start=1
+                        )
+                    }
+                    with open(f"{BASE_DIR}/todo_list_data.json", "w") as update:
+                        json.dump(todo_file_read, fp=update)
+
+                else:
+                    return False  # This will be used later for ctx.send
+        else:
+            return False  # This will be used later for ctx.send
+
+        return True
 
 
 class TodoList(Cog):
@@ -86,7 +124,7 @@ class TodoList(Cog):
 
         if task_type == "todo_list":
             self.add_to_todo_list_tasks[ctx.author.id] += 1
-        elif task_type == "delete_todo":
+        elif task_type == "delete_todos":
             self.delete_from_todo_list_tasks[ctx.author.id] += 1
 
         @tasks.loop(count=1)
@@ -109,18 +147,47 @@ class TodoList(Cog):
                 )
                 await ctx.send(embed=embed_msg)
 
-            self.add_to_todo_list_tasks[ctx.author.id] -= 1
+                self.add_to_todo_list_tasks[ctx.author.id] -= 1
+
+            elif task_type == "delete_todos":
+                seek = deletion_of_todos(ctx=ctx, keys_to_delete=todos)
+                if seek:
+                    embed_msg = Embed(
+                        title=f"{ctx.author}, todos have been deleted",
+                        description="Please check using `.show_todos`. Woof!",
+                    )
+                    await ctx.send(embed=embed_msg)
+                else:
+                    embed_msg = Embed(
+                        title=f"{ctx.author}, failed to delete.",
+                        description="Probably file failed to initialize or you have not created a todo list, "
+                        "yet.\nPlease check using `.show_todos`. Woof!",
+                    )
+                    await ctx.send(embed=embed_msg)
+
+                self.delete_from_todo_list_tasks[ctx.author.id] -= 1
 
         delay_for_completion.start()
 
     @command(brief="Friendo will create a new todo list for you.")
     async def todo_list(self, ctx, *, todos=None):
-        """Creates a todolist for the user."""
+        """Creates a todo list for the user."""
 
         if ctx.author.id not in self.add_to_todo_list_tasks:
             self.add_to_todo_list_tasks[ctx.author.id] = 0
         if self.add_to_todo_list_tasks[ctx.author.id] < 1:
             await self.todo_list_wrapper(ctx=ctx, task_type="todo_list", todos=todos)
+
+    @command(brief="Friendo deletes todos by specified keys")
+    async def delete_todos(self, ctx, *, todos=None):
+        """Deletes todos from a todo list for the user."""
+
+        if ctx.author.id not in self.delete_from_todo_list_tasks:
+            self.delete_from_todo_list_tasks[ctx.author.id] = 0
+        if (
+            self.delete_from_todo_list_tasks[ctx.author.id] < 1
+        ):  # todos here are keys not long strings.
+            await self.todo_list_wrapper(ctx=ctx, task_type="delete_todos", todos=todos)
 
     @command(brief="Friendo will present you your todo list.")
     async def show_todos(self, ctx):
@@ -136,29 +203,44 @@ class TodoList(Cog):
                 with open(f"{BASE_DIR}/todo_list_data.json", "r+") as read:
                     todo_file_read = json.load(read)
                     if str(ctx.author.id) in todo_file_read:
-                        listings = "".join(
-                            [
-                                f"{num}: {todo}\n"
-                                for num, todo in todo_file_read[
-                                    str(ctx.author.id)
-                                ].items()
-                            ]
-                        )
-                        embed_show_todos = Embed(
-                            title=f"Your todo list is here, {ctx.author}",
-                            description=listings,
-                            color=Colour.blurple(),
-                        )
-                        await ctx.send(embed=embed_show_todos)
+                        if todo_file_read[str(ctx.author.id)]:
+                            listings = "".join(
+                                [
+                                    f"{num}: {todo}\n"
+                                    for num, todo in todo_file_read[
+                                        str(ctx.author.id)
+                                    ].items()
+                                ]
+                            )
+                            embed_show_todos = Embed(
+                                title=f"Your todo list is here, {ctx.author}",
+                                description=listings,
+                                color=Colour.blurple(),
+                            )
+                            await ctx.send(embed=embed_show_todos)
+                        else:
+                            await ctx.send(
+                                embed=Embed(
+                                    title="You have no existing entries! Grrrr!",
+                                    color=Colour.red(),
+                                )
+                            )
                     else:
                         await ctx.send(
-                            embed=Embed(title="You have no existing entries! Grrrr!")
+                            embed=Embed(
+                                title="You have no existing entries! Grrrr!",
+                                color=Colour.red(),
+                            )
                         )
             else:
-                await ctx.send(embed=Embed(title="Todo list file empty! Arf!"))
+                await ctx.send(
+                    embed=Embed(title="Todo list file empty! Arf!", color=Colour.red())
+                )
 
         else:
-            await ctx.send(embed=Embed(title="Todo list file does not exist!"))
+            await ctx.send(
+                embed=Embed(title="Todo list file does not exist!", color=Colour.red())
+            )
 
 
 def setup(bot: Bot) -> None:
