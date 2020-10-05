57f0b3c9a64956b778e5cc0ae119f1a280ee839d
EgnaroDev2 <47803678+EgnaroDev2@users.noreply.github.com>
2020-10-05 13:30:33 -0700

    Update guess_number.py

--- bot/cogs/guess_number.py
+++ bot/cogs/guess_number.py
@@ -1,16 +1,12 @@
 import discord
 import math, random
-from discord.ext.commands import Bot, Cog, command # Comment this out when running this file alone.
-#from discord.ext import commands # Comment this out when this is a part of Cog file.
+from discord.ext.commands import Bot, Cog, command
 
-#bot_prefix = "."
-
-#client = commands.Bot(command_prefix=bot_prefix)
 
 class GuessGame(Cog): #(Cog): # Include Cog if it's a part of Cog file in Friendo Bot. Change class name if needed.
     """Commands for Guessing Right Number"""
 
-    def __init__(self, bot: Bot): # Comment this out when running this file alone.
+    def __init__(self, bot: Bot):
         self.bot = bot
 
     # Variables in class GuessGame
@@ -18,31 +14,21 @@ class GuessGame(Cog): #(Cog): # Include Cog if it's a part of Cog file in Friend
     lower_bound = 1
     upper_bound = 10
 
-    check_number = round(random.randint(lower_bound, upper_bound))  # Get random number.
+    check_number = round(random.randint(lower_bound, upper_bound))
     rounds_number = round(math.log(upper_bound - lower_bound + 1,
-                                   2))  # Get random number of rounds. Like 3 rounds or 3 attempts left to guess.
+                                   2))
 
     # Look in `guess` command below.
     count = 0
 
-    ##Results in terminal # Comment this out when this is a part of Cog file.
-    #@client.event
-    #async def on_ready(): # No `self` if no Cog.
-    #    print('Bot is ready.')
-
     ### Commands
-    # Change to @command(...) if this is a part of Cog. Otherwise, @client.command(...)
     @command(
         brief="- 1-10 Bound is Default. See `.help bound` for more details.",
         description = " - `.bound n1 n2`\nn1 => lower_bound, n2 => upper_bound\n Example: `.bound 1 10` will set the bound to 1 and 10.\nTo make Bound: 1-10 default, please type `.reset` to restart the bot."
-        #description = " - Enter `.bound 0 0` to get the default.\n`.bound n1 n2`\nn1 => lower_bound, n2 => upper_bound\n Example: `.bound 1 10` will set the bound to 1 and 10."
     )
-    # Set up the bounds.  Optional.  (1,10) is the default.
-    async def bound(self, ctx, lower: int,upper: int): # No `self` if no Cog. Will set bound when called.
-        # Default bounds unless it's rewritten with .bound command
-        #lower_bound = 1
-        #upper_bound = 10
 
+    # Set up the bounds.  Optional.  (1,10) is the default.
+    async def bound(self, ctx, lower: int,upper: int):
         if lower < upper:
             # Change default bounds
             GuessGame.lower_bound = lower
@@ -54,7 +40,7 @@ class GuessGame(Cog): #(Cog): # Include Cog if it's a part of Cog file in Friend
 
             await ctx.send(f'You entered bounds: {GuessGame.lower_bound} and {GuessGame.upper_bound}\nPlease `.start` or `.guess n`', delete_after=40)
 
-        elif lower > upper:
+        elif lower >= upper:
             await ctx.send(f'Sorry! The bound is wrong. Try again.', delete_after=30)
 
     # Starting the game...
@@ -62,16 +48,16 @@ class GuessGame(Cog): #(Cog): # Include Cog if it's a part of Cog file in Friend
         brief=" - Type `.start` to start the game.",
         description = " - Type `.start` to get more details on how play the game."
     )
-    async def start(self, ctx): # No `self` if no Cog.
+    async def start(self, ctx):
         await ctx.send(f'Try to guess a random number between {GuessGame.lower_bound}-{GuessGame.upper_bound}.\nType a number after `.guess` command.\nGood Luck!', delete_after=30)
 
-
+    # Testing the guess number...
     @command(
         brief=" - Enter the number after `.guess` to guess the right number.",
         description = " - Example: `.guess 5` and repeat until you guess it right."
     )
-    async def guess(self, ctx, guess_number: int): # No `self` if no Cog.
-        await ctx.send(f'Bounds: {GuessGame.lower_bound}, {GuessGame.upper_bound}') #To see if bounds are set properly.
+    async def guess(self, ctx, guess_number: int):
+        await ctx.send(f'Bounds: {GuessGame.lower_bound}, {GuessGame.upper_bound}', delete_after=30) #To see if bounds are set properly. Keeping it since it's useful for users to see their bounds.
 
         check_number = GuessGame.check_number
         rounds_number = GuessGame.rounds_number
@@ -95,17 +81,20 @@ class GuessGame(Cog): #(Cog): # Include Cog if it's a part of Cog file in Friend
         description=" - Make everything default."
     )
     async def reset(self, ctx):
+        # Make everything default.
         GuessGame.lower_bound = 1
         GuessGame.upper_bound = 10
 
-        GuessGame.check_number = round(random.randint(GuessGame.lower_bound, GuessGame.upper_bound))  # Get random number.
+        GuessGame.check_number = round(random.randint(GuessGame.lower_bound, GuessGame.upper_bound))
         GuessGame.rounds_number = round(math.log(GuessGame.upper_bound - GuessGame.lower_bound + 1,
-                                       2))  # Get random number of rounds. Like 3 rounds or 3 attempts left to guess.
+                                       2))
+
         GuessGame.count = 0
-        await ctx.send(f'Thank you for restarting.  Please `.start` to play again.')
+
+        await ctx.send(f'Thank you for restarting.  Please `.start` to play again or use `.help` to see options.')
 
 def setup(bot: Bot) -> None:
     """Load the Guess_Game cog."""
-    bot.add_cog(GuessGame(bot))  # # Comment this out when running this file, without Cog, alone.
+    bot.add_cog(GuessGame(bot))
 
-#client.run('token') for test running as a bot with this file alone.  Make sure to do this without `Cog` and `Bot`
+#client.run('token')
