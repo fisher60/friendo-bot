db78705c36c392ba9e170ed7b63a46d6056746c7
EgnaroDev2 <47803678+EgnaroDev2@users.noreply.github.com>
2020-10-05 13:52:50 -0700

    Update guess_number.py

--- bot/cogs/guess_number.py
+++ bot/cogs/guess_number.py
@@ -3,13 +3,13 @@ import math, random
 from discord.ext.commands import Bot, Cog, command
 
 
-class GuessGame(Cog): #(Cog): # Include Cog if it's a part of Cog file in Friendo Bot. Change class name if needed.
+class guess_game(Cog): #(Cog): # Include Cog if it's a part of Cog file in Friendo Bot. Change class name if needed.
     """Commands for Guessing Right Number"""
 
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    # Variables in class GuessGame
+    # Variables in class guess_game
     #Default bounds, random number, and rounds.
     lower_bound = 1
     upper_bound = 10
@@ -31,14 +31,14 @@ class GuessGame(Cog): #(Cog): # Include Cog if it's a part of Cog file in Friend
     async def bound(self, ctx, lower: int,upper: int):
         if lower < upper:
             # Change default bounds
-            GuessGame.lower_bound = lower
-            GuessGame.upper_bound = upper
+            guess_game.lower_bound = lower
+            guess_game.upper_bound = upper
 
             # Get new random number and rounds.
-            GuessGame.check_number = round(random.randint(GuessGame.lower_bound, GuessGame.upper_bound))  # Get random number.
-            GuessGame.rounds_number = round(math.log(GuessGame.upper_bound - GuessGame.lower_bound + 1,2))  # Get random number of rounds.
+            guess_game.check_number = round(random.randint(guess_game.lower_bound, guess_game.upper_bound))  # Get random number.
+            guess_game.rounds_number = round(math.log(guess_game.upper_bound - guess_game.lower_bound + 1,2))  # Get random number of rounds.
 
-            await ctx.send(f'You entered bounds: {GuessGame.lower_bound} and {GuessGame.upper_bound}\nPlease `.start` or `.guess n`', delete_after=40)
+            await ctx.send(f'You entered bounds: {guess_game.lower_bound} and {guess_game.upper_bound}\nPlease `.start` or `.guess n`', delete_after=40)
 
         elif lower >= upper:
             await ctx.send(f'Sorry! The bound is wrong. Try again.', delete_after=30)
@@ -49,7 +49,7 @@ class GuessGame(Cog): #(Cog): # Include Cog if it's a part of Cog file in Friend
         description = " - Type `.start` to get more details on how play the game."
     )
     async def start(self, ctx):
-        await ctx.send(f'Try to guess a random number between {GuessGame.lower_bound}-{GuessGame.upper_bound}.\nType a number after `.guess` command.\nGood Luck!', delete_after=30)
+        await ctx.send(f'Try to guess a random number between {guess_game.lower_bound}-{guess_game.upper_bound}.\nType a number after `.guess` command.\nGood Luck!', delete_after=30)
 
     # Testing the guess number...
     @command(
@@ -57,21 +57,21 @@ class GuessGame(Cog): #(Cog): # Include Cog if it's a part of Cog file in Friend
         description = " - Example: `.guess 5` and repeat until you guess it right."
     )
     async def guess(self, ctx, guess_number: int):
-        await ctx.send(f'Bounds: {GuessGame.lower_bound}, {GuessGame.upper_bound}', delete_after=30) #To see if bounds are set properly. Keeping it since it's useful for users to see their bounds.
+        await ctx.send(f'Bounds: {guess_game.lower_bound}, {guess_game.upper_bound}', delete_after=30) #To see if bounds are set properly. Keeping it since it's useful for users to see their bounds.
 
-        check_number = GuessGame.check_number
-        rounds_number = GuessGame.rounds_number
+        check_number = guess_game.check_number
+        rounds_number = guess_game.rounds_number
 
-        GuessGame.count += 1
+        guess_game.count += 1
 
         if check_number == guess_number:
             await ctx.send(f'Congratulations you did it! You entered {guess_number}.\nPlease enter `.reset` to restart the bot. Type `.start` to play again or use `.help` to see options.')
-            GuessGame.count -= rounds_number # So we don't bother passing `4th` if statement.
+            guess_game.count -= rounds_number # So we don't bother passing `4th` if statement.
         elif check_number > guess_number:
-            await ctx.send(f'You guessed too small!  Try again {rounds_number - GuessGame.count} round(s) pending.', delete_after=30)
+            await ctx.send(f'You guessed too small!  Try again {rounds_number - guess_game.count} round(s) pending.', delete_after=30)
         elif check_number < guess_number:
-            await ctx.send(f'You guessed too high!  Try again. {rounds_number - GuessGame.count} round(s) pending.', delete_after=30)
-        if GuessGame.count >= rounds_number:
+            await ctx.send(f'You guessed too high!  Try again. {rounds_number - guess_game.count} round(s) pending.', delete_after=30)
+        if guess_game.count >= rounds_number:
             await ctx.send(f'Sorry! The number is {check_number}. Please enter `.reset` to restart the bot. Type `.start` to play again.')
 
 
@@ -82,19 +82,19 @@ class GuessGame(Cog): #(Cog): # Include Cog if it's a part of Cog file in Friend
     )
     async def reset(self, ctx):
         # Make everything default.
-        GuessGame.lower_bound = 1
-        GuessGame.upper_bound = 10
+        guess_game.lower_bound = 1
+        guess_game.upper_bound = 10
 
-        GuessGame.check_number = round(random.randint(GuessGame.lower_bound, GuessGame.upper_bound))
-        GuessGame.rounds_number = round(math.log(GuessGame.upper_bound - GuessGame.lower_bound + 1,
+        guess_game.check_number = round(random.randint(guess_game.lower_bound, guess_game.upper_bound))
+        guess_game.rounds_number = round(math.log(guess_game.upper_bound - guess_game.lower_bound + 1,
                                        2))
 
-        GuessGame.count = 0
+        guess_game.count = 0
 
         await ctx.send(f'Thank you for restarting.  Please `.start` to play again or use `.help` to see options.')
 
 def setup(bot: Bot) -> None:
     """Load the Guess_Game cog."""
-    bot.add_cog(GuessGame(bot))
+    bot.add_cog(guess_game(bot))
 
 #client.run('token')
