b5f3d9f2cdab77ca4443e83491070633fd763b63
Sid200026 <siddharthsingharoy@gmail.com>
2020-10-10 22:33:39 +0530

    feat(): Disabling of certain cogs based on command line arguments

---
+++ bot/disable.py
@@ -0,0 +1,48 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+# no-api argument can be used to disable api cogs ie. events and memes currently
+parser.add_argument(
+    "--no-api",
+    help="disable api cogs in the bot",
+    action="store_true"
+)
+# enable argument can be used to enable certain cogs and disable all others
+parser.add_argument(
+    "--enable",
+    help="enable certain cogs in the bot",
+    default=[],
+    nargs="+"
+)
+# disable argument can be used to disable certain cogs and enable all others
+parser.add_argument(
+    "--disable",
+    help="disable certain cogs in the bot",
+    default=[],
+    nargs="+"
+)
+
+
+class DisableApi:
+    def __init__(self):
+        self.args = parser.parse_args()
+        # Both enable and disable arguments can be passed
+        if self.args.enable and self.args.disable:
+            raise ValueError("Cannot pass both enable and disable")
+
+    def get_no_api(self):
+        return self.args.no_api
+
+    def get_enable(self):
+        return self.args.enable
+
+    def get_disable(self):
+        return self.args.disable
+
+
+if __name__ == '__main__':
+    api_disabler = DisableApi()
+    print(api_disabler.get_no_api())
+    print(api_disabler.get_enable())
+    print(api_disabler.get_disable())
