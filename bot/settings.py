d760166ebef801eb259503b65e5ed64068a16639
fisher60 <wakefollower@gmail.com>
2020-10-22 18:03:58 -0700

    Fix: configure docker for logs
    Second commit, removed files from other branch.

--- bot/settings.py
+++ bot/settings.py
@@ -24,6 +24,6 @@ IMG_CACHE = Path(BASE_DIR, "image_cache")
 
 BASE_GITHUB_REPO = "https://github.com/fisher60/Friendo_Bot"
 
-LOG_FILE_NAME = "friendo.log"
+LOG_FILE_PATH = Path(BASE_DIR, "logs")
 
-API_COGS = ['events', 'memes']
+API_COGS = ["events", "memes"]
