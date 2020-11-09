3f7303af17af054e9311c9833930ad1a69ef1daa
mcurranseijo <68172072+mcurranseijo@users.noreply.github.com>
2020-11-09 16:39:47 -0600

    Update last_fm.py
    Linting

--- bot/cogs/last_fm.py
+++ bot/cogs/last_fm.py
@@ -7,142 +7,139 @@ from discord.ext import commands
 
 
 class MusicCog(commands.Cog):
-    """
-    Commands for song searching
-    """
+    """Commands for song searching."""
     def __init__(self, bot):
         self.bot = bot
-
    
     @commands.command(
-      name="getsong",
-      brief="a command to get song lyrics and info",
-      description="takes in song by itself or song followed by artist seperated by ; ",
-      aliases=['song', 'gets','getlyrics','getl']
+        name="getsong",
+         brief="a command to get song lyrics and info",
+        description="takes in song by itself or song followed by artist seperated by ; ",
+        aliases=['song', 'gets', 'getlyrics', 'getl']
     )
-    async def get_lyrics(self, ctx, *, args):
-        #"""Function to return discord embed with song info and lyrics"""
-        #try:
-                if ' ; ' in args:
-                    args = args.split('; ')
-                    data = await get_track(args[0], args[1])
-                else:
-                    data = await get_track(args)
-                track = urllib.parse.quote(data['name']).lower()
-                artist = urllib.parse.quote(data['artist']['name']).lower()
+    async def get_lyrics(self, ctx, *, args: str):
+        """Function to return discord embed with song info and lyrics."""
+        try:
+            if ';' in args:
+                args = args.split('; ')
+                data = await get_track(args[0], args[1])
+            else:
+                data = await get_track(args)
+            track = urllib.parse.quote(data['name']).lower()
+            artist = urllib.parse.quote(data['artist']['name']).lower()
 
-                async with aiohttp.ClientSession() as session:
-                  async with session.get('https://api.lyrics.ovh/v1/'+artist+'/'+track) as resp:
+            async with aiohttp.ClientSession() as session:
+                async with session.get('https://api.lyrics.ovh/v1/'+artist+'/'+track) as resp:
                     lyrics = (await resp.json())['lyrics']
 
-                split_lyrics = lyrics.split('\n\n\n')
-                print(split_lyrics)
-                lyric_array = [""]
-                print(len(lyric_array))
-                count = 0
-                for  f in split_lyrics:
-                  if len(lyric_array[count]+f) <= 256:
-                    lyric_array[count]+=f
-                  else:
-                    count+=1
-                    lyric_array.append(f)
-                  
-                
-                embed = discord.Embed(title=f"{data['name']} by {data['artist']['name']}", url=data['url'])
-                embed.add_field(name='Artist', value=data['artist']['name'])
-                embed.add_field(name='Album', value=data['album']['title'])
-                for f in lyric_array:
-                  if f:
-                    embed.add_field(name="\u200b",value=f,inline=False)
-                embed.set_thumbnail(url=data['album']['image'][2]['#text'])
-                await ctx.send(embed=embed)
-        #except IndexError:
-        #        await ctx.send('Invalid search term, try again')
-
-    @commands.command(
-      name="getalbum",
-      brief="gets info about an album",
-      description="takes in album by itself or album followed by artist seperated by ; ",
-      aliases=['album', 'getal']
-    )
-    async def getalbum(self, ctx, *, args):
-        """Function to return discord embed with album info"""
-        try:
-                if '; ' in args:
-                    args = args.split('; ')
-                    data = await get_album(args[0], args[1])
-                else:
-                    data = await get_album(args)
-                print(data)
-                embed = discord.Embed(title=f"{data['album']['name']} by {data['album']['artist']}", url=data['album']['url'])
-                embed.add_field(name='Artist', value=data['album']['artist'])
-                embed.set_thumbnail(url=data['album']['image'][2]['#text'])
-                try:
-                    embed.add_field(name='Release Data', value=data['album']['wiki']['published'], inline=False)
-                except KeyError:
-                    pass
-                try:
-                    embed.add_field(name='About', value=data['album']['wiki']['summary'].split('<a', 1)[0])
-                except KeyError:
-                    pass
-                await ctx.send(embed=embed)
+            split_lyrics = lyrics.split('\n\n\n')
+            print(split_lyrics)
+            lyric_array = [""]
+            print(len(lyric_array))
+            count = 0
+            for f in split_lyrics:
+              if len(lyric_array[count] + f) <= 256:
+                lyric_array[count] += f
+              else:
+                count += 1
+                lyric_array.append(f)
+ 
+            embed = discord.Embed(title=f"{data['name']} by {data['artist']['name']}", url=data['url'])
+            embed.add_field(name='Artist', value=data['artist']['name'])
+            embed.add_field(name='Album', value=data['album']['title'])
+            for f in lyric_array:
+              if f:
+                embed.add_field(name="\u200b", value=f, inline=False)
+            embed.set_thumbnail(url=data['album']['image'][2]['#text'])
+            await ctx.send(embed=embed)
         except IndexError:
-                await ctx.send('Invalid search term, try again')
+            await ctx.send('Invalid search term, try again')
 
     @commands.command(
-      name="getartist",
-      brief="A command that get's info about an artist",
-      description="Takes in just artist name",
-      aliases=['artist', 'getar']
+        name="getalbum",
+        brief="gets info about an album",
+        description="takes in album by itself or album followed by artist seperated by ; ",
+        aliases=['album', 'getal']
     )
-    async def getartist(self, ctx, *, args):
-        """Function to return discord embed with artist info"""
+    async def getalbum(self, ctx, *, args: str):
+        """Function to return discord embed with album info."""
         try:
-                data = await get_artist(args)              
-                album_data = (await get_data('artist.gettopalbums&artist='+urllib.parse.quote(data['name'])))['topalbums']['album']
-                bio = data['bio']['summary'].split('\n', 2)[0].split('<a', 1)[0]
-                if bio == '':
-                    bio = data['bio']['summary'].split('\n', 2)[1].split('<a', 1)[0]
-                embed = discord.Embed(title=data['name'], url=data['url'], description=bio)
-                embed.set_thumbnail(url=album_data[0]['image'][2]['#text'])
-                top_albums = "\n".join([x["name"] for x in album_data[:10]])
-                similar = "\n".join([x["name"] for x in data["similar"]["artist"]])
-                if not similar:
-                    similar = 'No Similar Artists Avaliable'
-                embed.add_field(name='Top Albums', value=top_albums, inline=True)
-                embed.add_field(name='Similar Artists', value=similar, inline=True)
-                await ctx.send(embed=embed)
+            if '; ' in args:
+                args = args.split('; ')
+                data = await get_album(args[0], args[1])
+            else:
+                data = await get_album(args)
+            print(data)
+            embed = discord.Embed(title=f"{data['album']['name']} by {data['album']['artist']}", url=data['album']['url'])
+            embed.add_field(name='Artist', value=data['album']['artist'])
+            embed.set_thumbnail(url=data['album']['image'][2]['#text'])
+            try:
+                embed.add_field(name='Release Data', value=data['album']['wiki']['published'], inline=False)
+            except KeyError:
+                pass
+            try:
+                embed.add_field(name='About', value=data['album']['wiki']['summary'].split('<a', 1)[0])
+            except KeyError:
+                pass
+            await ctx.send(embed=embed)
         except IndexError:
-                await ctx.send('Invalid search term, try again')
+            await ctx.send('Invalid search term, try again')
 
     @commands.command(
-      name="topsongs",
-      brief="Gets a list of the top songs on the world charts",
-      aliases=['songs', 'tops']
+        name="getartist",
+        brief="A command that get's info about an artist",
+        description="Takes in just artist name",
+        aliases=['artist', 'getar']
+    )
+    async def getartist(self, ctx, *, args: str):
+        """Function to return discord embed with artist info."""
+        try:
+            data = await get_artist(args)
+            album_data = (await get_data('artist.gettopalbums&artist='+urllib.parse.quote(data['name'])))['topalbums']['album']
+            bio = data['bio']['summary'].split('\n', 2)[0].split('<a', 1)[0]
+            if bio == '':
+                bio = data['bio']['summary'].split('\n', 2)[1].split('<a', 1)[0]
+            embed = discord.Embed(title=data['name'], url=data['url'], description=bio)
+            embed.set_thumbnail(url=album_data[0]['image'][2]['#text'])
+            top_albums = "\n".join([x["name"] for x in album_data[:10]])
+            similar = "\n".join([x["name"] for x in data["similar"]["artist"]])
+            if not similar:
+                similar = 'No Similar Artists Avaliable'
+            embed.add_field(name='Top Albums', value=top_albums, inline=True)
+            embed.add_field(name='Similar Artists', value=similar, inline=True)
+            await ctx.send(embed=embed)
+        except IndexError:
+            await ctx.send('Invalid search term, try again')
+
+    @commands.command(
+        name="topsongs",
+        brief="Gets a list of the top songs on the world charts",
+        aliases=['songs', 'tops']
     )
     async def topsongs(self, ctx):
-        """Function to return discord embed with top chart songs"""
+        """Function to return discord embed with top chart songs."""
         data = await top_tracks()
         embed = discord.Embed(title='Top 10 Tracks', url='https://www.last.fm/charts')
         for count, song in enumerate(data[:10], 1):
-                embed.add_field(name=str(count), value=f"{song['name']} by {song['artist']['name']}", inline=False)
+            embed.add_field(name=str(count), value=f"{song['name']} by {song['artist']['name']}", inline=False)
         await ctx.send(embed=embed)
 
     @commands.command(
-      name="topartists",
-      brief="Gets a list of the top artists on the world charts",
-      aliases=['artists', 'topa']
+        name="topartists",
+        brief="Gets a list of the top artists on the world charts",
+        aliases=['artists', 'topa']
     )
     async def topartists(self, ctx):
-        """Function to return discord embed with top chart artists"""
+        """Function to return discord embed with top chart artists."""
         data = await top_artists()
         embed = discord.Embed(title='Top 10 Artists', url='https://www.last.fm/charts')
         for count, artist in enumerate(data[:10], 1):
-                embed.add_field(name=str(count), value=artist['name'], inline=False)
+            embed.add_field(name=str(count), value=artist['name'], inline=False)
         await ctx.send(embed=embed)
 
-async def get_data(url_data1:str, url_data2:str=''):
-    """returns the json data from the url, takes in the two pieces of a URL as outlined in the last.fm api docs"""
+                                
+async def get_data(url_data1: str, url_data2: str = ''):
+    """Returns the json data from the url, takes in the two pieces of a URL as outlined in the last.fm api docs."""
     data = None
     async with aiohttp.ClientSession() as session:
         async with session.get(f"http://ws.audioscrobbler.com/2.0/?method={url_data1}&api_key={MUSIC_TOKEN}&format=json{url_data2}") as resp:
@@ -150,28 +147,27 @@ async def get_data(url_data1:str, url_data2:str=''):
     return data
 
 
-async def get_album(album:str, artist:str=''):
-        """returns the json data for a given album, takes in just an album or album and artist"""
+async def get_album(album: str, artist: str = ''):
+        """Returns the json data for a given album, takes in just an album or album and artist."""
         if not artist:
-                album = urllib.parse.quote(album)
-                data = (await get_data('album.search&album='+album))
-                album = data['results']['albummatches']['album'][0]['name']
-                artist = data['results']['albummatches']['album'][0]['artist']
-                return await get_album(album, artist)
+            album = urllib.parse.quote(album)
+            data = (await get_data('album.search&album='+album))
+            album = data['results']['albummatches']['album'][0]['name']
+            artist = data['results']['albummatches']['album'][0]['artist']
+            return await get_album(album, artist)
         else:
-                artist = urllib.parse.quote(artist)
-                album = urllib.parse.quote(album)
-                return await get_data('album.getinfo', '&artist='+artist+'&album='+album)
+            artist = urllib.parse.quote(artist)
+            album = urllib.parse.quote(album)
+            return await get_data('album.getinfo', '&artist='+artist+'&album='+album)
 
 
-async def get_artist(artist:str):
+async def get_artist(artist: str):
     """Returns the json data for a given artist, takes in artist name."""
-
     artist = urllib.parse.quote(artist)
     return (await get_data('artist.getinfo&artist='+artist))['artist']
 
 
-async def get_track(track: str, artist: str= ''):
+async def get_track(track: str, artist :str = ''):
     """Returns info about a specific song/track takes in just a track or artist and track."""
     artist = urllib.parse.quote(artist)
     track = urllib.parse.quote(track)
@@ -182,6 +178,7 @@ async def get_track(track: str, artist: str= ''):
         artist = (await get_data('track.search&track='+track))['results']['trackmatches']['track'][0]['artist']
         track = (await get_data('track.search&track='+track))['results']['trackmatches']['track'][0]['name']
         return await get_track(track, artist)
+
 
 async def top_tracks():
     """Returns data of the top songs on the charts."""
