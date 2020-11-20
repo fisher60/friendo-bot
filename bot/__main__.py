60776270dea5b9a60ada5586c33ed1039d473d22
mcurranseijo <68172072+mcurranseijo@users.noreply.github.com>
2020-11-20 16:41:22 -0600

    Update __main__.py
    removed blank space from line 50

--- bot/__main__.py
+++ bot/__main__.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
 
     # loads the weather cog
     bot.load_extension("bot.cogs.weather")
-    
+
     # loads the animals cog
     bot.load_extension("bot.cogs.animals")
 
