9f3927e0f1acf264f35c7d3b4fd3ff9ebbca9391
saiTama-max <vinamraj01@gmail.com>
2020-11-26 18:01:57 +0530

    Added few more features and updated code

--- bot/cogs/pixelate.py
+++ bot/cogs/pixelate.py
@@ -1,6 +1,8 @@
+import numpy as np
 from PIL import Image
 from io import BytesIO
 from bot.bot import Friendo
+from collections import Counter
 from discord import Color, Embed, File, Member
 from discord.ext.commands import Cog, Context, command
 
@@ -11,17 +13,30 @@ class Pixelate(Cog):
     def __init__(self, bot: Friendo) -> None:
         self.bot = bot
 
+    @staticmethod
+    def dominant_color(image: Image) -> tuple:
+        """Get the most occuring color in the image in rgb form as a tuple."""
+        image = image.convert("RGB")
+        arr = np.array(image)
+        ls = []
+        for pixel in arr:
+            for rgb in pixel:
+                ls.append(tuple(rgb))
+        return Counter(ls).most_common(1)[0][0]
+
     @command(brief="Shows the pixelated avatar of the user/author",
              usage=".pixelate [user (optional)]",
              aliases=['pixel', 'blockify', 'pix'])
     async def pixelate(self, ctx: Context, user: Member = None) -> None:
         """Pixelate command, takes in an optional parameter user else pixelates author's avatar."""
         async with ctx.channel.typing():
-            user = user
-            img_bytes = ctx.author.avatar_url.read() if not user else user.avatar_url.read()
+            user = ctx.author.avatar_url if not user else user.avatar_url
+            img_bytes = user.read()
             image = Image.open(BytesIO(await img_bytes))
 
-            img_small = image.resize((16, 16), resample=Image.BILINEAR)
+            img_color = self.dominant_color(image)
+
+            img_small = image.resize((24, 24), resample=Image.BILINEAR)
             result = img_small.resize((1024, 1024), Image.NEAREST)
 
             buffer = BytesIO()
@@ -31,8 +46,8 @@ class Pixelate(Cog):
             img_file = File(buffer, filename="pixelated.png")
             img_url = 'attachment://pixelated.png'
 
-            img_emb = Embed(color=Color.blue())
-            img_emb.set_footer(text="Here is your pixelated image")
+            img_emb = Embed(color=Color.from_rgb(int(img_color[0]), int(img_color[1]), int(img_color[2])))
+            img_emb.set_author(name="Here is your pixelated Image", icon_url=user)
             img_emb.set_image(url=img_url)
 
             await ctx.send(embed=img_emb, file=img_file)
