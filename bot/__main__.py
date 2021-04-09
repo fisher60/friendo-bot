196dd4b808316bf42b46105315f42a7532378006
GDWR <gregory.dwr@gmail.com>
2021-04-09 23:20:25 +0100

    Add cog loading, This validates the setup function is present.

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,6 +1,32 @@
+import inspect
+import pkgutil
+from typing import Iterator, NoReturn
+
 from discord import AllowedMentions, Intents
+
 from . import settings
 from bot.bot import Friendo
+from bot import cogs
+
+
+def _get_cogs() -> Iterator[str]:
+    """
+    Return an iterator going through each cog,
+    On each iteration the cog is check for having a
+    setup function raising a more readable error if not found.
+    """
+
+    def on_error(name: str) -> NoReturn:
+        raise ImportError(name=name)
+
+    for module in pkgutil.walk_packages(cogs.__path__, f"{cogs.__name__}.", onerror=on_error):
+        if module.ispkg:
+            _import = __import__(module.name)
+            if not inspect.isfunction(getattr(_import, "setup", None)):
+                continue
+
+        yield module.name
+
 
 if __name__ == "__main__":
     bot = Friendo(
@@ -8,60 +34,7 @@ if __name__ == "__main__":
         allowed_mentions=AllowedMentions(everyone=False),
     )
 
-    # load help command
-    bot.load_extension("bot.cogs.help")
-
-    # load in basic commands
-    bot.load_extension("bot.cogs.greetings")
-    bot.load_extension("bot.cogs.utilities")
-    bot.load_extension("bot.cogs.source")
-
-    # load in image segmentation commands
-    bot.load_extension("bot.cogs.image_segmentation")
-
-    # load in Meme commands
-    bot.load_extension("bot.cogs.memes")
-
-    # load in Admin commands
-    bot.load_extension("bot.cogs.admin")
-
-    # load in Fun commands
-    bot.load_extension("bot.cogs.fun")
-
-    # load in randomcase command
-    bot.load_extension("bot.cogs.randomcase")
-
-    # load in Event command
-    bot.load_extension("bot.cogs.events")
-
-    # load in Todo List command
-    bot.load_extension("bot.cogs.todo_list")
-
-    # load in wolfram command
-    bot.load_extension("bot.cogs.wolfram")
-
-    # load covid stats
-    bot.load_extension("bot.cogs.covid_stats")
-
-    # load music cogs
-    bot.load_extension("bot.cogs.last_fm")
-
-    # load wonder twins cog
-    bot.load_extension("bot.cogs.wonder_twins")
-
-    # loads the weather cog
-    bot.load_extension("bot.cogs.weather")
-
-    # loads the pixelate cog
-    bot.load_extension("bot.cogs.pixelate")
-
-    # loads the advent of code cog
-    bot.load_extension("bot.cogs.advent_of_code")
-
-    # loads the user cog
-    bot.load_extension("bot.cogs.user")
-
-    # loads the dogeboard cog
-    bot.load_extension("bot.cogs.dogeboard")
+    for cog in _get_cogs():
+        bot.load_extension(cog)
 
     bot.run(settings.TOKEN)
