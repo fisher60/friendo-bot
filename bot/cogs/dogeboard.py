c7392786ca692d828ede4de5872c4a51a1253d21
GDWR <gregory.dwr@gmail.com>
2021-04-10 23:34:51 +0100

    Pre-commit Flake8

--- bot/cogs/dogeboard.py
+++ bot/cogs/dogeboard.py
@@ -4,11 +4,11 @@ from dataclasses import dataclass
 from typing import Any, Dict, Optional, Tuple
 
 from aiohttp import ClientResponse
-from discord import Color, Embed, Member, RawReactionActionEvent, Reaction, TextChannel, Message
+from discord import Color, Embed, Member, Message, RawReactionActionEvent, Reaction, TextChannel
 from discord.ext.commands import Cog, Context, group
 
 from bot import settings
-from bot.bot import Bot, Friendo
+from bot.bot import Friendo
 
 log = logging.getLogger(__name__)
 
@@ -24,12 +24,16 @@ class QueryError(Exception):
 
 @dataclass
 class DogeBoardData:
-    """Dataclass to hold the data retrieved from the API."""
+    """
+    Dataclass to hold the data retrieved from the API.
+
+    Names are made to match the API field names.
+    """
 
     guild_id: int
-    channel_id: int
-    emoji: str
-    reactions_required: int
+    dogeboard_id: int
+    dogeboard_emoji: str = "🐶"
+    dogeboard_reactions_required: int = 5
 
 
 class DogeBoard(Cog):
@@ -74,68 +78,79 @@ class DogeBoard(Cog):
             except QueryError as e:
                 log.error(f"Couldn't login: {e}")
 
-    async def _get_guild_data(self, guild_id: int) -> DogeBoardData:
+    async def _get_guild_data(self, guild_id: int) -> Optional[DogeBoardData]:
         """
         Attempt to get the dogeboard_data from cache using guild_id.
 
         if the data is not in the cache, it will request it from the GraphQL DB.
         if the data doesn't exist in the database it will be created and send to GraphQL DB
         """
+        if self._token is None:
+            await self._login()
+
         if dogeboard_data := self._cache.get(guild_id):
             return dogeboard_data
         elif dogeboard_data := await self._fetch_guild_data(guild_id):
             return dogeboard_data
-        else:
-            return await self._create_new_dogeboard(guild_id)
 
     async def _fetch_guild_data(self, guild_id: int) -> Optional[DogeBoardData]:
         """Attempt to fetch guild data from the GraphQL database."""
         query = """mutation {
-            server(data: { server_id: "%d", }) {
+            get_guild(data: { guild_id: "%s", }) {
+                guild_id
                 dogeboard_id
                 dogeboard_emoji
                 dogeboard_reactions_required
               }
-            }
-           """ % guild_id
+        }""" % guild_id
 
         async with self.bot.session.post(self._url, json={"query": query}, headers=self.headers) as resp:
             try:
                 data = await self._handle_request(resp)
-                dogeboard_data = DogeBoardData(**data)
+
+                dogeboard_data = DogeBoardData(**data["data"]["get_guild"])
                 self._cache[guild_id] = dogeboard_data
                 return dogeboard_data
             except QueryError as e:
                 log.error(f"Query Errors: {e}")
 
-    async def _create_new_dogeboard(self, guild_id: int) -> DogeBoardData:
-        """Create a new guild DogeBoard, this is sent to the GraphQL DB."""
+    async def _update_guild_data(self, dogeboard: DogeBoardData) -> None:
+        """Update the cache and api with the new dogeboard data."""
+        if self._token is None:
+            await self._login()
+
+        self._cache[dogeboard.guild_id] = dogeboard
         query = """mutation {
-            server(data: { server_id: "%d", }) {
-                dogeboard_id
-                dogeboard_emoji
-                dogeboard_reactions_required
-              }
-            }
-           """ % guild_id
+            modify_guild(data: {
+                guild_id: %d,
+                dogeboard_id: %d,
+                dogeboard_emoji: "%s",
+                dogeboard_reactions_required: %d
+            }) {
+              guild_id
+          }
+        }""" % (
+            dogeboard.guild_id,
+            dogeboard.dogeboard_id,
+            dogeboard.dogeboard_emoji,
+            dogeboard.dogeboard_reactions_required
+        )
 
         async with self.bot.session.post(self._url, json={"query": query}, headers=self.headers) as resp:
             try:
-                data = await self._handle_request(resp)
-                return DogeBoardData(**data)
+                await self._handle_request(resp)
+                self._cache[dogeboard.guild_id] = dogeboard
             except QueryError as e:
                 log.error(f"Query Errors: {e}")
 
-    async def send_dogeboard_message(self, message: Message, channel: TextChannel):
-        author: Member = message.author
-
+    @staticmethod
+    async def _send_dogeboard_message(message: Message, channel: TextChannel) -> None:
         embed = Embed(
-            title=author.display_name,
+            title=message.author.display_name,
             description=message.content + f"\n\n[Jump to message]({message.jump_url})",
-            colour=author.colour,
+            colour=message.author.colour,
         )
-
-        embed.set_thumbnail(url=author.avatar_url)
+        embed.set_thumbnail(url=message.author.avatar_url)
 
         # Set the image to the first one if there is one
         for attachment in message.attachments:
@@ -145,69 +160,118 @@ class DogeBoard(Cog):
                 break
 
         await channel.send(embed=embed)
+        await message.add_reaction("✅")
 
     @Cog.listener()
     async def on_raw_reaction_add(self, payload: RawReactionActionEvent) -> None:
         """Handle event when users add reactions."""
-
         # Ensure the caches are ready
         await self.bot.wait_until_ready()
 
-        if self._token is None:
-            await self._login()
+        dogeboard_data = await self._get_guild_data(payload.guild_id)
+        if not dogeboard_data:
+            return
 
-        # Test data
-        # dogeboard_data = self._get_guild_data(payload.guild_id)
-        dogeboard_data = DogeBoardData(
-            guild_id=566407576686952480,
-            channel_id=830213225806430208,
-            emoji="💩",
-            reactions_required=5,
-        )
-
-        if str(payload.emoji) == dogeboard_data.emoji:
+        if str(payload.emoji) == dogeboard_data.dogeboard_emoji:
 
             channel = self.bot.get_channel(payload.channel_id)
             message = await channel.fetch_message(payload.message_id)
-            reaction = next(r for r in message.reactions if r.emoji == str(payload.emoji))
 
-            if reaction.count >= dogeboard_data.reactions_required:
-                dogeboard_channel = self.bot.get_channel(dogeboard_data.channel_id)
-                await self.send_dogeboard_message(message, dogeboard_channel)
+            dogeboard_reaction: Optional[Reaction] = None
+            for reaction in message.reactions:
 
+                if reaction.emoji == "✅":
+                    if self.bot.user in await reaction.users().flatten():
+                        # If the bot has set a checkmark reaction to this message,
+                        # it's already been DogeBoarded
+                        return
+
+                elif reaction.emoji == payload.emoji:
+                    dogeboard_reaction = reaction
+
+            if dogeboard_reaction:
+                if dogeboard_reaction.count >= dogeboard_data.dogeboard_reactions_required:
+                    dogeboard_channel = self.bot.get_channel(dogeboard_data.dogeboard_id)
+                    await self._send_dogeboard_message(message, dogeboard_channel)
 
     @group(brief="Collate the best message of the server using reactions")
     async def dogeboard(self, ctx: Context) -> None:
         """Command group for DogeBoard."""
         if not ctx.subcommand_passed:
-            embed = Embed(title="DogeBoard", description="commands", color=Color.orange())
-            embed.add_field(name="emoji", value="Set the emoji used for the DogeBoard")
-            embed.add_field(name="channel", value="Set the DogeBoard channel")
-            embed.add_field(name="required", value="Set the reactions required for the DogeBoard")
+            dogeboard_data = await self._get_guild_data(ctx.guild.id)
+            embed = Embed(title="DogeBoard", color=Color.blurple())
+
+            if not dogeboard_data:
+                embed.description = "DogeBoard is not configured, " \
+                                    "use `.dogeboard channel <channel>` to start."
+            else:
+                embed.add_field(name="Emoji", value=dogeboard_data.dogeboard_emoji)
+                embed.add_field(name="Channel", value=ctx.guild.get_channel(dogeboard_data.dogeboard_id))
+                embed.add_field(name="Required", value=str(dogeboard_data.dogeboard_reactions_required))
+
             await ctx.send(embed=embed)
 
-    @dogeboard.command(brief="Set the emoji used for the DogeBoard, Custom Emojis Only")
+    @dogeboard.command(brief="Set the dogeboard_emoji used for the DogeBoard, Custom Emojis Only")
     async def emoji(self, ctx: Context) -> None:
-        """Set the emoji used for the DogeBoard."""
-        reaction_msg = await ctx.send(content="> React with the new emoji")
+        """Set the dogeboard_emoji used for the DogeBoard."""
+        dogeboard_data = await self._get_guild_data(ctx.guild.id)
+
+        if not dogeboard_data:
+            await ctx.send("> DogeBoard has not been configured for this guild. "
+                           "Use `.dogeboard channel <channel>` to enable")
+            return
+
+        reaction_msg = await ctx.send(content="> React with the new dogeboard_emoji")
 
         def check(reaction: Reaction, member: Member) -> bool:
             return member == ctx.author and reaction_msg == reaction.message
 
         try:
-            reaction, member = await self.bot.wait_for("reaction_add", check=check, timeout=5)
+            reaction, member = await self.bot.wait_for("reaction_add", check=check, timeout=60)
         except asyncio.TimeoutError:
-            return await reaction_msg.edit(content="> You didn't react, Canceling emoji change.")
+            return await reaction_msg.edit(content="> You didn't react, Canceling dogeboard_emoji change.")
         finally:
-            await reaction_msg.delete(delay=5)
+            await reaction_msg.delete(delay=10)
+
+        # Default Emoji or Guild dogeboard_emoji
+        if not reaction.custom_emoji or reaction.emoji in ctx.guild.emojis:
+            dogeboard_data.dogeboard_emoji = reaction.emoji
+        else:
+            await ctx.send("> Please react with a default or guild dogeboard_emoji.")
+            return None
+
+        await ctx.send(f"> Updated your emoji to {dogeboard_data.dogeboard_emoji}")
+        await self._update_guild_data(dogeboard_data)
+
+    @dogeboard.command(brief="Set the channel for DogeBoard messages to be sent to.")
+    async def channel(self, ctx: Context, channel: TextChannel) -> None:
+        """Set the channel used for the DogeBoard."""
+        dogeboard_data = await self._get_guild_data(ctx.guild.id)
+
+        if not dogeboard_data:
+            dogeboard_data = DogeBoardData(guild_id=ctx.guild.id, channel_id=channel.id)
+        else:
+            dogeboard_data.dogeboard_id = channel.id
+
+        await self._update_guild_data(dogeboard_data)
+        await ctx.send(f"> Updated the channel for DogeBoard to: {channel.mention}", delete_after=5)
 
     @dogeboard.command(brief="Set the reactions required for the DogeBoard")
     async def required(self, ctx: Context, amount: int) -> None:
         """Set the amount of required emojis to trigger the DogeBoard."""
-        await ctx.send(str(amount))
+        dogeboard_data = await self._get_guild_data(ctx.guild.id)
+
+        if not dogeboard_data:
+            await ctx.send("> DogeBoard has not been configured for this guild. "
+                           "Use `.dogeboard channel <channel>` to enable")
+            return
+
+        dogeboard_data.dogeboard_reactions_required = amount
+        await self._update_guild_data(dogeboard_data)
+        await ctx.send(f"> Updated DogeBoard required reactions to: {amount}", delete_after=5)
 
 
-def setup(bot: Bot) -> None:
+def setup(bot: Friendo) -> None:
     """Adding the help cog."""
     if not settings.FRIENDO_API_USER:
         raise EnvironmentError("Missing environment variable: FRIENDO_API_USER")
